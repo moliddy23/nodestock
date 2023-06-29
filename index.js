3b1fe9c34d3cd7fa31c1eb475483cb7a0ce6edd3
Maurice Liddy <moliddy23@yahoo.com>
2023-06-29 08:43:06 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const PORT = process.env.PORT || 5000;
+const path = require('path');
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine','handlebars');
+
+app.get('/',function(req,res){
+    res.render('home',{
+        stuff: 'This is stuff'
+    });
+});
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, ()=> console.log('Server listening on Port:',PORT));
